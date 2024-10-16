7e27844a93edaa11ae113660aa5d81a3af787e8f
Arjan Dikhoff <dikhoff@gmail.com>
2024-10-16 20:40:01 +0200

    Try git clone

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ INPUT_DIR=$1
 OUTPUT_DIR=$2
 
 MIG_DIR="./_migrated"
+BUILD_DIR="redot-docs-build"
 
 touch test.txt 
 echo 'hello' >> test.txt 
@@ -14,9 +15,9 @@ cp test.txt $OUTPUT_DIR/index.html
 python migrate.py --tiny $INPUT_DIR $MIG_DIR
 sphinx-build -b html -j 4 $MIG_DIR $OUTPUT_DIR
 
-# do we have node?
-node --version
+git clone git@github.com:Redot-Engine/$BUILD_DIR.git
 
-# do we have git?
-git --version
+cp -r $MIG_OUTPUT_DIR/* $BUILD_DIR/en/master
 
+cd $BUILD_DIR
+ls -la
